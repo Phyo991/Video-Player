62c6f82f4393969a401a5fb96d3cef6f88ee7590
Phyo Theingi <142148113+Phyo991@users.noreply.github.com>
2023-09-20 15:43:24 +0700

    Add files via upload

---
+++ video_player/video_player/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Phyo Thiengi  on 10/09/2023.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+            super.viewDidAppear(animated)
+            playVideo()
+    }
+    
+        
+    
+    private func playVideo() {
+        guard let path=Bundle.main.path(forResource: "test", ofType:"mp4") else{
+            debugPrint("Test not found")
+            return
+        }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerController = AVPlayerViewController()
+        playerController.player=player
+            present(playerController, animated: true) {
+        player.play()
+        }
+    }
+
+}
+
